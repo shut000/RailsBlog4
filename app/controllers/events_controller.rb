cca8bc88cd4977d0c0a9a27c05001461223accfe
shut000 <gwlpro@163.com>
2014-11-19 09:24:00 +0800

    change

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,43 @@ class EventsController < ApplicationController
 	end
 
 	def show
-		@events = Event.find(params[:id])
+		@event = Event.find(params[:id])
 	end
+
+	def new
+		@event=Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		@event.save
+
+		redirect_to :action=>:index
+	end
+
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		@event.update(event_params)
+
+		redirect_to :action => :show, :id =>@event
+	end
+
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+
+		redirect_to :action => :index
+	end
+
+	private
+
+	def event_params
+	  params.require(:event).permit(:name, :description)
+	end
+
 
 end
